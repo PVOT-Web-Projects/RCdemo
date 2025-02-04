170f94ed8ae31837db0e2bdfeef379bc0269aba8
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2025-02-04 18:17:24 +0530

    ds

--- src/components/HomeSliderOne/page.jsx
+++ src/components/HomeSliderOne/page.jsx
@@ -381,7 +381,7 @@ const SwiperCarousel = () => {
                 <video
                   //  ./video/Kitchen Interior _ Royal Crown.mp4
                   //  https://interiormaataassets.humbeestudio.xyz/videos/Kitchen%20Interior%20_%20Royal%20Crown.mp4
-                  src="./video/kitchen_new.mp4"
+                  src="https://vanras.humbeestudio.xyz/videos/kitchen_new.mp4"
                   type="video/mp4"
                   autoPlay
                   playsInline
@@ -412,7 +412,7 @@ const SwiperCarousel = () => {
                 <video
                   //  ./video/Kitchen Interior _ Royal Crown.mp4
                   //  https://interiormaataassets.humbeestudio.xyz/videos/Kitchen%20Interior%20_%20Royal%20Crown.mp4
-                  src="./video/living_new.mp4"
+                  src="https://vanras.humbeestudio.xyz/videos/living_new.mp4"
                   type="video/mp4"
                   autoPlay
                   playsInline
@@ -443,7 +443,7 @@ const SwiperCarousel = () => {
                 <video
                   //  ./video/Kitchen Interior _ Royal Crown.mp4
                   //  https://interiormaataassets.humbeestudio.xyz/videos/Kitchen%20Interior%20_%20Royal%20Crown.mp4
-                  src="./video/interiorOut_new.mp4"
+                  src="https://vanras.humbeestudio.xyz/videos/interiorOut_new.mp4"
                   type="video/mp4"
                   autoPlay
                   playsInline
@@ -474,7 +474,7 @@ const SwiperCarousel = () => {
                 <video
                   //  ./video/Kitchen Interior _ Royal Crown.mp4
                   //  https://interiormaataassets.humbeestudio.xyz/videos/Kitchen%20Interior%20_%20Royal%20Crown.mp4
-                  src="./video/living_new.mp4"
+                  src="https://vanras.humbeestudio.xyz/videos/living_new.mp4"
                   type="video/mp4"
                   autoPlay
                   playsInline
@@ -505,7 +505,7 @@ const SwiperCarousel = () => {
                 <video
                   //  ./video/Kitchen Interior _ Royal Crown.mp4
                   //  https://interiormaataassets.humbeestudio.xyz/videos/Kitchen%20Interior%20_%20Royal%20Crown.mp4
-                  src="./video/interiorOut_new.mp4"
+                  src="https://vanras.humbeestudio.xyz/videos/interiorOut_new.mp4"
                   type="video/mp4"
                   autoPlay
                   playsInline
